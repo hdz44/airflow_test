ec1931bc6049da983b85d002c4892b5f24e24886
hdz44 <144772190+hdz44@users.noreply.github.com>
2024-04-26 14:29:20 +0200

    Update sales_data_extraction.py

--- sales_data_extraction.py
+++ sales_data_extraction.py
@@ -12,7 +12,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 2,
-    'retry_delay': timedelta(minutes=1),
+    'retry_delay': timedelta(minutes=2),
 }
 """hello world!"""
 dag = DAG(
