509821cd029fff470da9b44bdf357a530a5d5530
hdz44 <144772190+hdz44@users.noreply.github.com>
2024-05-07 11:26:59 +0200

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # Define the DAG
 dag = DAG(
-    dag_id="Sarvandani_dag",
+    dag_id="test_dag",
     description="just dag!'",
     start_date=datetime(2023, 7, 1),
     schedule_interval="@daily",
